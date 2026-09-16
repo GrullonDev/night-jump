5f6ac94b584211c30ffa3b95829c050b3b46a3d8
Jorge-Tropigas <jorge.grullon@tomzagroup.com>
2026-09-16 15:24:43 -0600

    feat: se agregan nuevas librerias para las animaciones y los obstaculos.

--- lib/utils/app.dart
+++ lib/utils/app.dart
@@ -6,15 +6,14 @@ import 'package:night_jump/utils/theme/app_color.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Night Jump (Demo)',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: AppColor.inversePrimary),
       ),
-      home: const HomePage() /* MyHomePage(title: 'Flutter Demo Home Page') */,
+      home: const HomePage(),
     );
   }
 }
